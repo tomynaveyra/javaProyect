4d90c38778c93f4ca62a72d5324ee1ad20f11321
Tomás Naveyra <tomynaveyra@gmail.com>
2022-08-24 00:35:41 -0300

    DOM

--- codigo.js
+++ codigo.js
@@ -1,86 +1,31 @@
 
-//for(intentos=1;intentos<=3;intentos++){
-//    let contrasenia=prompt("Ingrese la Contraseña");
-//    if(contrasenia==1234){
-//        console.log("Bienvenido a Tienda Ritual");
-//        break;
-//    }else{
-//        console.log("La contraseña es Incorrecta")
-//    }
-//}
-//let total = 0;
-//let producto1 = 500;
-//let producto2 = 600;
+let lista=document.getElementById("contenedor-pagina");
+let carrito = [];
+crearProductos();
 
-//function comprar(producto1,porducto2){
-//    total = producto1 + producto2
-//    console.log("la suma de $ "+producto1+" + $"+porducto2+" es igual $"+ total);
-    
-//}
-//comprar(producto1,producto2);
-
-//function comprarEfectivo(producto1){
-//    totalEfectivo= producto1*0.8;
-//    console.log("El producto1 en efectivo sale $"+totalEfectivo);
-//}
-//comprarEfectivo(producto1);
-
-
-
-//Array de proyecto
-
-class producto {
-    constructor(nombre, categoria, precio, disponibilidad) {
-        this.nombre = nombre
-        this.categoria = categoria
-        this.precio = precio
-        this.disponibilidad = disponibilidad
+function crearProductos(){
+    for(const producto of productos) {
+        lista.innerHTML+=`<div class="contenedor-productos">
+        <h3>${producto.nombre}</h3>
+        <img src=${producto.foto} alt="">
+        <h4>$${producto.precio}</h4>
+        <button class="boton-add"id="btn${producto.id}">Comprar</button>
+    </div>`;
     }
-    agregarStock(){
-        let error = "producto disponible"
-        if(this.disponibilidad == false){
-            this.disponibilidad = true
-        }else{
-            console.log(error)
-        }
-    }
-    retirarStock(){
-        let error = "producto no disponible"
-        if(this.disponibilidad == true){
-            this.disponibilidad = false
-        }else{
-            console.log(error)
-        }
-    }
+    productos.forEach(producto => {
+        document.getElementById(`btn${producto.id}`).addEventListener(`click`,function(){
+            agregarAlCarrito(producto);
+        })
+    });
 }
-
-let producto1 = new producto("energetica","vela",120,true)
-let porducto2 = new producto("purificadora","vela",150,true)
-let producto3 = new producto("cuarzo","piedra",200,false)
-let producto4 = new producto("opalo","piedra",220,true)
-let producto5 = new producto("salvia","hierba",90,true)
-let producto6 = new producto("lavanda","hierba",120,true)
-
-const tienda = []
-tienda.push(producto1,porducto2,producto3,producto4,producto5,producto6)
-
-let piedras = tienda.filter(producto=>producto.categoria == "piedra")
-console.table(piedras)
-
-let disponible = tienda.filter(producto=>producto.disponibilidad == true)
-console.table(disponible)
-
-let encuentra = tienda.find(producto=>producto.nombre == "cuarzo")
-console.table(encuentra)
-
-let productoIva = tienda.map(producto=>{
-    return {
-        nombre: producto.nombre,
-        categoria: producto.categoria,
-        precio: producto.precio * 1.21,
-        disponibilidad: producto.disponibilidad
-
-    }
-})
-
-console.table(productoIva)
+function agregarAlCarrito(producto){
+    carrito.push(producto);
+    console.log(carrito);
+    document.getElementById("items").innerHTML+=`
+        <tr>
+            <td>${producto.id}</td>
+            <td>${producto.nombre}</td>
+            <td>$${producto.precio}</td>
+        </tr> 
+    `;
+}
---
+++ productos.js
@@ -0,0 +1,43 @@
+const productos = [{
+    nombre: "energética",
+    categoria: "vela",
+    precio: 120,
+    foto: "./img/velaPrendida.jpeg",
+    id: 1,
+},
+{
+    nombre: "purificadora",
+    categoria: "vela",
+    precio: 150,
+    foto: "./img/velasPlateadas.jpeg",
+    id: 2,
+},
+{
+    nombre: "cuarzo",
+    categoria: "piedra",
+    precio: 200,
+    foto: "./img/piedrasYEstatuilla.jpeg",
+    id: 3,
+},
+{
+    nombre: "opalo",
+    categoria: "piedra",
+    precio: 220,
+    foto: "./img/piedrasEnMesa.jpeg",
+    id: 4,
+},
+{
+    nombre: "salvia",
+    categoria: "hierba",
+    precio: 90,
+    foto: "./img/rellenoDeSaumerioSolo.jpeg",
+    id: 5,
+},
+{
+    nombre: "lavanda",
+    categoria: "hierba",
+    precio: 120,
+    foto: "./img/saumerioConRelleno.jpeg",
+    id: 6,
+},
+]
