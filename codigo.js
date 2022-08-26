bb017368e3a1d28d80b647fcd0ec083ad5b969e4
Tomás Naveyra <tomynaveyra@gmail.com>
2022-08-25 23:56:18 -0300

    agrego storage

--- codigo.js
+++ codigo.js
@@ -1,16 +1,20 @@
 
 let lista=document.getElementById("contenedor-pagina");
 let carrito = [];
+if(localStorage.getItem("carritoCompras")!= null){
+    carrito=JSON.parse(localStorage.getItem("carritoCompras"));
+}
+
 crearProductos();
 
 function crearProductos(){
     for(const producto of productos) {
-        lista.innerHTML+=`<div class="contenedor-productos">
+        lista.innerHTML +=`<div class="contenedor-productos">
         <h3>${producto.nombre}</h3>
         <img src=${producto.foto} alt="">
         <h4>$${producto.precio}</h4>
         <button class="boton-add"id="btn${producto.id}">Comprar</button>
-    </div>`;
+        </div>`;
     }
     productos.forEach(producto => {
         document.getElementById(`btn${producto.id}`).addEventListener(`click`,function(){
@@ -26,6 +30,9 @@ function agregarAlCarrito(producto){
             <td>${producto.id}</td>
             <td>${producto.nombre}</td>
             <td>$${producto.precio}</td>
+            <button class="delete" scope="row">X</button>
         </tr> 
     `;
+
+localStorage.setItem("carroCompras",JSON.stringify(carrito));
 }
